f8a9c95c84460fec08bff99f2074acaab0532b77
donavon <github@donavon.com>
2015-08-13 10:52:07 -0400

    Tests

---
+++ test/env.js
@@ -0,0 +1,10 @@
+var chai = require("chai");
+var sinonChai = require("sinon-chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+chai.should();
+
+global.sinon = require("sinon");
+global.expect = chai.expect;
